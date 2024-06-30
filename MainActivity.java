73354795e119b239ba274d321102cad47561ae67
Patrick De Wolf <138595746+lupotec@users.noreply.github.com>
2024-06-30 23:55:30 +0200

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,53 @@
+package be.app.blank;
+/*
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
+*/
+
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the WebView by its unique ID
+        WebView webView = findViewById(R.id.web);
+
+        // loading https://www.geeksforgeeks.org url in the WebView.
+        webView.loadUrl("https://www.geeksforgeeks.org");
+
+        // this will enable the javascript.
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // WebViewClient allows you to handle
+        // onPageFinished and override Url loading.
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
